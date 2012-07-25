dd443427e7c38f66043007b91e421a61644bab3b
Daniel Dimitrov <daniel@compojoom.com>
2012-07-25 11:18:00 +0200

    we don't need a settings table anymore

--- source/administrator/components/com_cmc/sql/install.mysql.sql
+++ source/administrator/components/com_cmc/sql/install.mysql.sql
@@ -1,13 +1,3 @@
-CREATE TABLE IF NOT EXISTS `#__cmc_settings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(500) NOT NULL,
-  `value` text NOT NULL,
-  `values` text NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `catdisp` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
 CREATE TABLE IF NOT EXISTS `#__cmc_lists` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `mc_id` varchar(10) NOT NULL,
