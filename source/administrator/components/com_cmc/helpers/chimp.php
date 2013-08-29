3d564ece912ff679fe4c23f32a5fa879ccd95a37
Daniel Dimitrov <daniel@compojoom.com>
2013-08-29 11:20:19 +0300

    phpcs

--- source/administrator/components/com_cmc/helpers/chimp.php
+++ source/administrator/components/com_cmc/helpers/chimp.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 22.07.12
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       28.08.13
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -11,21 +11,34 @@ defined('_JEXEC') or die('Restricted access');
 JLoader::register('MCAPI', JPATH_ADMINISTRATOR . '/components/com_cmc/libraries/mailchimp/MCAPI.class.php');
 
 /**
+ * Class cmcHelperChimp
+ *
  * This class will work as a small abstraction over the MCAPI class.
  * I got too tired of typing the $key all the time :)
+ *
+ * @since  1.0
  */
-class cmcHelperChimp extends MCAPI {
+class CmcHelperChimp extends MCAPI
+{
+	/**
+	 * The constructor
+	 *
+	 * @param   string  $key     - the mailchimp api key
+	 * @param   string  $secure  - use secure connection
+	 */
+	public function __construct($key = '', $secure = '')
+	{
+		if (!$key)
+		{
+			$key = JComponentHelper::getParams('com_cmc')->get('api_key', '');
+		}
 
-    public function __construct($key = '', $secure = '' ) {
-        if(!$key) {
-            $key = JComponentHelper::getParams('com_cmc')->get('api_key', '');
-        }
+		if (!$secure)
+		{
+			$config = JFactory::getConfig();
+			$secure = $config->get('force_ssl', 0);
+		}
 
-        if(!$secure) {
-            $config = JFactory::getConfig();
-            $secure = $config->get('force_ssl', 0);
-        }
-
-        parent::MCAPI($key, $secure);
-    }
+		parent::MCAPI($key, $secure);
+	}
 }
