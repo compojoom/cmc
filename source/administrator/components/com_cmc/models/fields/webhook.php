e015396322cc786e88e35112c2d355e94999d708
Daniel Dimitrov <daniel@compojoom.com>
2013-09-07 16:57:58 +0300

    phpcs

--- source/administrator/components/com_cmc/models/fields/webhook.php
+++ source/administrator/components/com_cmc/models/fields/webhook.php
@@ -1,59 +1,67 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 22.07.12
+ * @package    Cmc
+ * @author     DanielDimitrov <daniel@compojoom.com>
+ * @date       07.09.13
  *
- * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('_JEXEC') or die('Restricted access');
 
-class JFormFieldWebhook extends JFormFieldText {
+/**
+ * Class JFormFieldWebhook
+ *
+ * @since  1.0
+ */
+class JFormFieldWebhook extends JFormFieldText
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 *
+	 * @since  11.1
+	 */
+	protected $type = 'Webhook';
 
-    /**
-     * The form field type.
-     *
-     * @var    string
-     *
-     * @since  11.1
-     */
-    protected $type = 'Webhook';
-
-    /**
-     * Method to get the field input markup.
-     *
-     * @return  string  The field input markup.
-     *
-     * @since   11.1
-     */
-    protected function getInput()
-    {
-        $script = "window.addEvent('domready', function() {
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		$script = "window.addEvent('domready', function() {
             document.id('webhook-url').addEvent('click', function() {
                 this.select();
             });
         });";
-        $document = JFactory::getDocument();
-        $document->addScriptDeclaration($script);
+		$document = JFactory::getDocument();
+		$document->addScriptDeclaration($script);
 
-        // Initialize some field attributes.
-        $size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
-        $maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
-        $class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
-        $readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
-        $disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+		// Initialize some field attributes.
+		$size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+		$maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
+		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
+		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 
-        // Initialize JavaScript field attributes.
-        $onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+		// Initialize JavaScript field attributes.
+		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
 
 
-        if(!$this->value) {
-            $this->value = md5(JFactory::getUser()->id . JFactory::getUser()->name . microtime());
-        }
+		if (!$this->value)
+		{
+			$this->value = md5(JFactory::getUser()->id . JFactory::getUser()->name . microtime());
+		}
 
-        return '<input type="hidden" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
-            . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . '/>
-            <input type="text" size="60" readonly="readonly" id="webhook-url" value="'.JURI::root().'index.php?option=com_cmc&format=raw&task=webhooks.request&key='.$this->value .'" />';
-    }
+		return '<input type="hidden" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+		. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . '/>
+            <input type="text" size="60" readonly="readonly" id="webhook-url" value="' . JURI::root()
+				. 'index.php?option=com_cmc&format=raw&task=webhooks.request&key=' . $this->value . '" />';
+	}
 }
