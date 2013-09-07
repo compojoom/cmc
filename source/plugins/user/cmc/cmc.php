b36963a3a9c3ceb5a03b4e7c02db6532ada8528d
Yves Hoppe <hoppe.yves@gmail.com>
2013-09-07 09:20:21 +0200

    Working on Joomla Plugin

--- source/plugins/user/cmc/cmc.php
+++ source/plugins/user/cmc/cmc.php
@@ -25,8 +25,8 @@ class PlgUserCmc extends JPlugin
 	/**
 	 * Prepares the data
 	 *
-	 * @param   string  $context  - the context
-	 * @param   object  $data     - the data object
+	 * @param   string $context - the context
+	 * @param   object $data - the data object
 	 *
 	 * @return bool
 	 */
@@ -34,13 +34,11 @@ class PlgUserCmc extends JPlugin
 	function onContentPrepareData($context, $data)
 	{
 		// Check we are manipulating a valid form.
-		if (!in_array($context, array('com_users.profile', 'com_users.user', 'com_users.registration', 'com_admin.profile')))
-		{
+		if (!in_array($context, array('com_users.profile', 'com_users.user', 'com_users.registration', 'com_admin.profile'))) {
 			return true;
 		}
 
-		if (is_object($data))
-		{
+		if (is_object($data)) {
 			// Extend form
 		}
 
@@ -50,8 +48,8 @@ class PlgUserCmc extends JPlugin
 	/**
 	 * Prepares the form
 	 *
-	 * @param   string  $form  - the form
-	 * @param   object  $data  - the data object
+	 * @param   string $form - the form
+	 * @param   object $data - the data object
 	 *
 	 * @return bool
 	 */
@@ -70,25 +68,28 @@ class PlgUserCmc extends JPlugin
 
 		if (!in_array(
 			$name, array('com_admin.profile', 'com_users.user',
-			'com_users.profile', 'com_users.registration'
-		)
+				'com_users.profile', 'com_users.registration'
+			)
 		))
 		{
 			return true;
 		}
 
 		// Example = $form->text(array("FNAME", "text", "First Name", 0, ""));
-		$field = array("FNAME", "text", "First Name", 0, "");
+		$fields = array("FNAME", "text", "First Name", 0, "");
+
+		$formcode = "<form>";
+		$formcode .= "<fields name=\"cmc\">";
 
 
-		//  ["@attributes"]=> array(8) { ["name"]=> string(4) "name" ["type"]=> string(4) "text"
-		// ["description"]=> string(28) "COM_USERS_REGISTER_NAME_DESC" ["filter"]=> string(6) "string"
-		// ["label"]=> string(29) "COM_USERS_REGISTER_NAME_LABEL" ["message"]=> string(31)
-		// "COM_USERS_REGISTER_NAME_MESSAGE" ["required"]=> string(4) "true" ["size"]=> string(2) "30" }
-		$form->setField(new JFormFieldText(array("name" => "testcmc", "type" => "text",
-			"description" => "blabla", "filter" => "string", "label" => "testcmc", "message" => "messagecmc", "required" => true, "size" => 30 )));
+
+		$formcode .= "</fields>";
+		$formcode .= "</form>";
 
 
+
+		// Inject fields into the form
+		$form->load($formcode);
 
 		return true;
 	}
@@ -96,9 +97,9 @@ class PlgUserCmc extends JPlugin
 	/**
 	 * Prepares the form
 	 *
-	 * @param   string   $user   - the not saved user obj
-	 * @param   boolean  $isNew  - is the user new
-	 * @param   object   $data   - the data object
+	 * @param   string $user - the not saved user obj
+	 * @param   boolean $isNew - is the user new
+	 * @param   object $data - the data object
 	 *
 	 * @return   void
 	 */
@@ -112,10 +113,10 @@ class PlgUserCmc extends JPlugin
 	/**
 	 * Prepares the form
 	 *
-	 * @param   object   $data    - the users data
-	 * @param   boolean  $isNew   - is the user new
-	 * @param   object   $result  - the db result
-	 * @param   string   $error   - the error message
+	 * @param   object $data - the users data
+	 * @param   boolean $isNew - is the user new
+	 * @param   object $result - the db result
+	 * @param   string $error - the error message
 	 *
 	 * @return   boolean
 	 */
@@ -124,8 +125,7 @@ class PlgUserCmc extends JPlugin
 	{
 		$userId = JArrayHelper::getValue($data, 'id', 0, 'int');
 
-		if ($userId && $result && isset($data['profile']) && (count($data['profile'])))
-		{
+		if ($userId && $result && isset($data['profile']) && (count($data['profile']))) {
 			// Save data
 		}
 
@@ -138,24 +138,22 @@ class PlgUserCmc extends JPlugin
 	 *
 	 * Method is called after user data is deleted from the database
 	 *
-	 * @param   array    $user     - Holds the user data
-	 * @param   boolean  $success  - True if user was succesfully stored in the database
-	 * @param   string   $msg      - Message
+	 * @param   array $user - Holds the user data
+	 * @param   boolean $success - True if user was succesfully stored in the database
+	 * @param   string $msg - Message
 	 *
 	 * @return boolean
 	 */
 
 	function onUserAfterDelete($user, $success, $msg)
 	{
-		if (!$success)
-		{
+		if (!$success) {
 			return false;
 		}
 
 		$userId = JArrayHelper::getValue($user, 'id', 0, 'int');
 
-		if ($userId)
-		{
+		if ($userId) {
 			// Delete User from mailing list?
 		}
 
