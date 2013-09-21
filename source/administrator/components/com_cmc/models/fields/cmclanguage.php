9f938bebb236c80f5e874f6a8346f48f22479b89
Daniel Dimitrov <daniel@compojoom.com>
2013-09-21 11:18:02 +0200

    adding translations and removing stuff we don't use

---
+++ source/administrator/components/com_cmc/models/fields/cmclanguage.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package    Cmc
+ * @author     DanielDimitrov <daniel@compojoom.com>
+ * @date       21.09.13
+ *
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_BASE') or die;
+
+$language = JFactory::getLanguage();
+$language->load('com_cmc', JPATH_ADMINISTRATOR, 'en-GB', true);
+$language->load('com_cmc', JPATH_ADMINISTRATOR, $language->getDefault(), true);
+$language->load('com_cmc', JPATH_ADMINISTRATOR, null, true);
+$language->load('mod_cmc', JPATH_SITE, null, true);
+$language->load('mod_cmc', JPATH_SITE, $language->getDefault(), true);
+$language->load('mod_cmc', JPATH_SITE, null, true);
+
+class JFormFieldCmcLanguage extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $type = 'HotspotsLanguage';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		return '';
+	}
+}
