da0a3fac2d5c4138e740f9019ac8c1b447e1eb36
Daniel Dimitrov <daniel@compojoom.com>
2014-07-07 10:04:39 +0200

    updated build scripts

---
+++ builds/templates/symlinks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    Hotspots
+ * @author     DanielDimitrov <daniel@compojoom.com>
+ * @date       02.11.13
+ *
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+// Path are relative from source directory of the current project
+$symlinks = array(
+	'/../../lib_compojoom/source/libraries' => '/libraries',
+	'/../../lib_compojoom/source/language' => '/language',
+	'/../../lib_compojoom/source/media' => '/media'
+);
