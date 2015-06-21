11c7e1376fd3a5e4475ea57e9556dc8986328516
Daniel Dimitrov <daniel@compojoom.com>
2015-06-21 15:14:57 +0200

    remove unused functions

--- source/administrator/components/com_cmc/helpers/basic.php
+++ source/administrator/components/com_cmc/helpers/basic.php
@@ -61,115 +61,6 @@ class CmcHelperBasic
 		return $db->loadObjectList();
 	}
 
-	/**
-	 * @static
-	 *
-	 * @param $api_key
-	 * @param $list_id
-	 * @param $email
-	 *
-	 * @return string
-	 */
-	public static function getUserDetailsMC($api_key, $list_id, $email, $id = null, $store = true)
-	{
-		$api = new MCAPI($api_key);
-		$appl = JFactory::getApplication();
-
-		$retval = $api->listMemberInfo($list_id, $email);
-
-		if ($api->errorCode)
-		{
-			JFactory::getApplication()->enqueueMessage(JTEXT::_("COM_CMC_LOAD_USER_FAILED") . " " . $api->errorCode . " / " . $api->errorMessage, 'error');
-
-			return false;
-		}
-		else
-		{
-			/**
-			 * @return array array of list members with their info in an array (see Returned Fields for details)
-			 * @returnf int success the number of subscribers successfully found on the list
-			 * @returnf int errors the number of subscribers who were not found on the list
-			 * @returnf array data an array of arrays where each one has member info:
-			string id The unique id for this email address on an account
-			string email The email address associated with this record
-			string email_type The type of emails this customer asked to get: html, text, or mobile
-			array merges An associative array of all the merge tags and the data for those tags for this email address.
-			 * <em>Note</em>: Interest Groups are returned as comma delimited strings - if a group name contains a comma,
-			 *          it will be escaped with a backslash. ie, "," =&gt; "\,". Groupings will be returned with their "id" and "name"
-			 *          as well as a "groups" field formatted just like Interest Groups
-			string status The subscription status for this email address, either pending, subscribed, unsubscribed, or cleaned
-			string ip_opt IP Address this address opted in from.
-			string ip_signup IP Address this address signed up from.
-			int member_rating the rating of the subscriber. This will be 1 - 5 as described <a href="http://eepurl.com/f-2P" target="_blank">here</a>
-			string campaign_id If the user is unsubscribed and they unsubscribed from a specific campaign, that campaign_id will be listed, otherwise this is not returned.
-			array lists An associative array of the other lists this member belongs to - the key is the list id and the value is their status in that list.
-			date timestamp The time this email address was added to the list
-			date info_changed The last time this record was changed. If the record is old enough, this may be blank.
-			int web_id The Member id used in our web app, allows you to create a link directly to it
-			array clients the various clients we've tracked the address as using - each included array includes client 'name' and 'icon_url'
-			array static_segments the 'id', 'name', and date 'added' for any static segment this member is in
-			 */
-
-			$item = array();
-
-			foreach ($retval['data'] as $user)
-			{
-				$item['id'] = $id;
-				$item['mc_id'] = $user['id'];
-
-				$item['list_id'] = $list_id;
-				$item['email_type'] = $user['email_type'];
-				$item['email'] = $user['email'];
-
-				$item['merges'] = json_encode($user['merges']);
-
-				$item['firstname'] = $user['merges']['FNAME'];
-				$item['lastname'] = $user['merges']['LNAME'];
-
-				//$item['interests'] = $user['merges']['INTERESTS'];
-
-				$item['status'] = $user['status'];
-				$item['ip_opt'] = $user['ip_opt'];
-				$item['ip_signup'] = $user['ip_signup'];
-				$item['language'] = $user['language'];
-
-				$item['member_rating'] = $user['member_rating'];
-
-				$item['timestamp'] = $user['timestamp'];
-				$item['info_changed'] = $user['info_changed'];
-
-				$item['web_id'] = $user['web_id'];
-				$item['clients'] = json_encode($user['clients']);
-				$item['static_segments'] = json_encode($user['static_segments']);
-
-				$item['lists'] = json_encode($user['lists']);
-
-
-				$item['query_data'] = json_encode($retval);
-
-				if ($store)
-				{
-					$row = JTable::getInstance('users', 'CmcTable');
-
-					try
-					{
-						$row->bind($item);
-						$row->check();
-						$row->store();
-					}
-					catch (Exception $e)
-					{
-						$appl->enqueueMessage(JText::_('COM_CMC_LIST_ERROR_SAVING') . $e->getMessage());
-
-						return false;
-					}
-				}
-			}
-
-			return $row;
-		}
-
-	}
 
 	/**
 	 * @static
@@ -184,7 +75,6 @@ class CmcHelperBasic
 	 */
 	public static function subscribeList($api_key, $list_id, $email, $firstname, $lastname, $user = null, $groupings = array(null), $email_type = "html", $update = false)
 	{
-
 		$api = new MCAPI($api_key);
 
 		$merge_vars = array_merge(array('FNAME' => $firstname, 'LNAME' => $lastname), $groupings);
@@ -196,6 +86,7 @@ class CmcHelperBasic
 		if ($api->errorCode)
 		{
 			JFactory::getApplication()->enqueueMessage(JTEXT::_("COM_CMC_SUBSCRIBE_FAILED") . " " . $api->errorCode . " / " . $api->errorMessage, 'error');
+
 			return false;
 		}
 		else
@@ -214,9 +105,10 @@ class CmcHelperBasic
 	 */
 	public static function unsubscribeList($user)
 	{
-		$api = new cmcHelperChimp();
+		$api = new cmcHelperChimp;
 
 		$api->listUnsubscribe($user->list_id, $user->email, true);
+
 		if ($api->errorCode)
 		{
 			throw new Exception(JTEXT::_("COM_CMC_UNSUBSCRIBE_FAILED") . ": " . $api->errorMessage, $api->errorCode);
@@ -292,52 +184,6 @@ class CmcHelperBasic
 		return implode($separator, $string);
 	}
 
-	/**
-	 * @static
-	 *
-	 * @param      $api_key
-	 * @param      $list_id
-	 * @param bool $optin
-	 * @param bool $up_exist
-	 * @param bool $replace_int
-	 *
-	 * @return string
-	 */
-	public static function subscribeListBatch($api_key, $list_id, $batchlist, $optin = true, $up_exist = true, $replace_int = false)
-	{
-		$api = new MCAPI($api_key);
-
-		// Todo check rights
-
-		$optin = true; //yes, send optin emails
-		$up_exist = true; // yes, update currently subscribed users
-		$replace_int = false; // no, add interest, don't replace
-
-		$vals = $api->listBatchSubscribe($list_id, $batchlist, $optin, $up_exist, $replace_int);
-
-		if ($api->errorCode)
-		{
-			JFactory::getApplication()->enqueueMessage(
-				JText::_("COM_CMC_UNSUBSCRIBE_FAILED") . " " . $api->errorCode . " / " . $api->errorMessage,
-				'error'
-			);
-			return false;
-		}
-		else
-		{
-			// Todo return this
-			echo "added:   " . $vals['add_count'] . "\n";
-			echo "updated: " . $vals['update_count'] . "\n";
-			echo "errors:  " . $vals['error_count'] . "\n";
-			foreach ($vals['errors'] as $val)
-			{
-				echo $val['email_address'] . " failed\n";
-				echo "code:" . $val['code'] . "\n";
-				echo "msg :" . $val['message'] . "\n";
-			}
-		}
-
-	}
 
 	/**
 	 * @param string $option
@@ -447,6 +293,4 @@ class CmcHelperBasic
 
 		return $menu;
 	}
-
-
 }
