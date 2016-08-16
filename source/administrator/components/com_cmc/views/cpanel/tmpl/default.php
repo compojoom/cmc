f9aee8ca8be32142eae4bdca0476082f5cf29bb1
Daniel Dimitrov <daniel@compojoom.com>
2016-08-16 15:46:43 +0300

    Using partners.compojoom.com to serve ads in the backend

--- source/administrator/components/com_cmc/views/cpanel/tmpl/default.php
+++ source/administrator/components/com_cmc/views/cpanel/tmpl/default.php
@@ -107,29 +107,7 @@ JHtml::_('behavior.tooltip');
 
 				<div class="text-center">
 					<!--/* Ads for our products */-->
-
-					<script type='text/javascript'><!--//<![CDATA[
-						var m3_u = (location.protocol == 'https:' ? 'https://matangazo.compojoom.com/www/delivery/ajs.php' : 'http://matangazo.compojoom.com/www/delivery/ajs.php');
-						var m3_r = Math.floor(Math.random() * 99999999999);
-						if (!document.MAX_used) document.MAX_used = ',';
-						document.write("<scr" + "ipt type='text/javascript' src='" + m3_u);
-						document.write("?zoneid=1");
-						document.write('&amp;cb=' + m3_r);
-						document.write('&amp;isPro=0');
-						if (document.MAX_used != ',') document.write("&amp;exclude=" + document.MAX_used);
-						document.write(document.charset ? '&amp;charset=' + document.charset : (document.characterSet ? '&amp;charset=' + document.characterSet : ''));
-						document.write("&amp;loc=" + escape(window.location));
-						if (document.referrer) document.write("&amp;referer=" + escape(document.referrer));
-						if (document.context) document.write("&context=" + escape(document.context));
-						if (document.mmm_fo) document.write("&amp;mmm_fo=1");
-						document.write("'><\/scr" + "ipt>");
-						//]]>--></script>
-					<noscript><a
-							href='http://matangazo.compojoom.com/www/delivery/ck.php?n=a8ed4360&amp;cb=INSERT_RANDOM_NUMBER_HERE'
-							target='_blank'><img
-								src='http://matangazo.compojoom.com/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a8ed4360'
-								border='0' alt=''/></a></noscript>
-
+					<script type="text/javascript" src="https://partners.compojoom.com/scripts/banner.php?a_aid=compojoom&a_bid=30f713ae"></script>
 				</div>
 			</div>
 		</div>
